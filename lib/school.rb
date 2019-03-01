67b6a6cfe8c83f0e475c948a66193ed4ccef62f1
Cody Yantis <cyantis@gmail.com>
2019-03-01 21:22:49 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,15 +1,15 @@
 class School
   attr_reader :name
 
-  ROSTER = {}
+  roster = {}
 
   def initialize(name)
     @name = name
   end
 
   def add_student(student_name, grade)
-    ROSTER[grade] = [] if !ROSTER.key?(grade)
-    ROSTER[grade] << student_name
+    roster[grade] = [] if !roster.key?(grade)
+    roster[grade] << student_name
   end
 
 end
