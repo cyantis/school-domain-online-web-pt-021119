943f19745f43c467f16b4c00d7870d16122ec963
Cody Yantis <cyantis@gmail.com>
2019-03-01 21:38:08 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -12,7 +12,7 @@ class School
   end
 
   def add_student(student_name, grade)
-    ROSTER[grade] ||= ROSTER[grade] = []
+    ROSTER[grade] = [] if !ROSTER.key?(grade)
     ROSTER[grade] << student_name
   end
 
